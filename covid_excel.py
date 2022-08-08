dabfa6a8fe14a9ce341b26fc81d79d184c19c59f
jairo neder monassa <84083197+desenvolvedorjairomonassa@users.noreply.github.com>
2022-08-08 20:52:27 -0300

    Create covid_excel.py

---
+++ covid_excel.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding, utf-8
+
+import sys
+import json
+import pyspark.sql.functions as f
+from pyspark.sql.functions import when, col
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType, DateType, TimestampType, DecimalType
+from pyspark import SparkContext, SparkConf, SQLContext, HiveContext
+from collections import namedtuple
+
+spark = SparkSession.builder \
+        .master("yarn") \
+        .enableHiveSupport() \
+        .getOrCreate()
+
+schema = StructType([ \
+    StructField("regiao",StringType(),True), \
+    StructField("estado",StringType(),True), \
+    StructField("municipio",StringType(),True), \
+    StructField("coduf", IntegerType(), True), \
+    StructField("codmun", IntegerType(), True), \
+    StructField("codRegiaoSaude", IntegerType(), True), \
+    StructField("nomeRegiaoSaude",StringType(),True), \
+    StructField("data",TimestampType(),True), \
+    StructField("semanaEpi",IntegerType(),True), \
+    StructField("populacaoTCU2019", IntegerType(), True), \
+    StructField("casosAcumulado", DecimalType(), True), \
+    StructField("casosNovos", IntegerType(), True) ,\
+    StructField("obitosAcumulado", IntegerType(), True), \
+    StructField("obitosNovos", IntegerType(), True), \
+    StructField("Recuperadosnovos", IntegerType(), True), \
+    StructField("emAcompanhamentoNovos", IntegerType(), True), \
+    StructField("interior/metropolitana", IntegerType(), True), \
+  ])
+
+df = spark.read.option("header","true")\
+ .option("schema",schema)\
+ .option("mode","DROPMALFORMED")\
+ .option("delimiter", ";")\
+ .option("inferSchema", "true")\
+ .csv("/user/jairomonassa/covid/excel/*.csv")
+
+
+df= df.withColumnRenamed("codRegiaoSaude","codregiaosaude")\
+	.withColumnRenamed("nomeRegiaoSaude","nomeregiaosaude")\
+	.withColumnRenamed("semanaEpi","semanaepi")\
+	.withColumnRenamed("populacaoTCU2019","populacaotcu2019")\
+	.withColumnRenamed("casosAcumulado","casosacumulado")\
+	.withColumnRenamed("casosNovos","casosnovos")\
+	.withColumnRenamed("obitosAcumulado","obitosacumulado")\
+	.withColumnRenamed("obitosNovos","obitosnovos")\
+	.withColumnRenamed("emAcompanhamentoNovos","emacompanhamentonovos")\
+	.withColumnRenamed("interior/metropolitana","interiormetropolitana")
+
+df.write.mode("overwrite").partitionBy("municipio").saveAsTable("covid")
